79bf93bfbde105fc7817edbc7162790b0baee084
shrikantbathe <shrikant.bathe@gmail.com>
2026-02-01 21:20:00 +0530

    inin

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+       // 0oazq8a5z2leAi4YF697
+
+
+
+      //  291Z9PhSkjgMjz2eKnnnUoNLNCDhhaySgjty9ky8sVTtwRTqwRUNKF22IuD90-X6
+        System.out.println("Hello world!");
+    }
+}
